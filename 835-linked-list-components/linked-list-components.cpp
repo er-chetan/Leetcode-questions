4dd200809e7f92dbd26928ab4b627c3aa94221f9
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-03-31 12:44:05 +0530

    Time: 34 ms (45.94%) | Memory: 25 MB (41.76%) - LeetSync

--- 835-linked-list-components/linked-list-components.cpp
+++ 835-linked-list-components/linked-list-components.cpp
@@ -1,4 +1,4 @@
-/**
+ /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -11,36 +11,27 @@
 class Solution {
 public:
     int numComponents(ListNode* head, vector<int>& nums) {
-        unordered_map<int,ListNode*> m;
+        int c=0;
         ListNode* temp=head;
-        while(temp!=NULL){
-            m[temp->val]=temp->next;
-            temp=temp->next;
-        }
-        unordered_set<int> s;
-        for(auto ele : nums){
-            s.insert(ele);
-        }
+        unordered_map<int,int> mp;
 
-        int count=0;
-        ListNode* nex=head;
-        bool find=false;
-        while(s.size()>0){
-            if(s.find(nex->val)!=s.end()){
-                find=true;
-                s.erase(nex->val);
-            }else{
-                if(find==true) count++;
-                find=false;
+        for(auto i:nums){
+            mp[i]++;
+        }
+         
+         while(temp!=NULL){
+            if(mp.find(temp->val)!=mp.end()){
+                 if(temp->next){
+                    if(mp.find(temp->next->val)==mp.end()){
+                       c++;
+                    }
+                }else{
+                   c++;
+                }
             }
-            nex=m[nex->val];
-        }
+             temp=temp->next;
+       }
 
-        if(find==true){
-            return count+1;
-        }
-
-        
-        return count;
+        return c;
     }
 };
