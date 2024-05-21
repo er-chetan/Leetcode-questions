2b60813c0964742ab8b676d02b8bdf68d025d2c8
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-05-21 09:01:25 +0530

    Time: 27 ms (66.07%) | Memory: 19.4 MB (67.53%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -1,28 +1,19 @@
-    bool cmp(pair<int,int>& p,pair<int,int>& q){
-        
-        return p.first>q.first;
+    bool cmp(vector<int> &a,vector<int>& b){
+        return a[1]>b[1];
     }
 
 class Solution {
 public:
     
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        vector<pair<int,int>> v;
         int result=0;
-        for(auto ele : boxTypes){
-            pair<int,int> p;
-            p.first=ele[1];
-            p.second=ele[0];
-            v.push_back(p);
-        }
-        sort(v.begin(),v.end(),cmp);
-        for(int i=0;i<v.size() && truckSize!=0 ;i++){
-            pair<int,int> temp=v[i];
-            if(temp.second<truckSize){
-                result+=(temp.second*temp.first);
-                truckSize-=temp.second;
+        sort(boxTypes.begin(),boxTypes.end(),cmp);
+        for(int i=0;i<boxTypes.size() && truckSize!=0 ;i++){
+            if(boxTypes[i][0]<truckSize){
+                result+=(boxTypes[i][1]*boxTypes[i][0]);
+                truckSize-=boxTypes[i][0];
             }else{
-                result+=(truckSize*temp.first);
+                result+=(truckSize*boxTypes[i][1]);
                 truckSize=0;
             }
         }
