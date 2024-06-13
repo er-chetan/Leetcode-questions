06f861bf403739e367c6a012b21e3c8b86fe751a
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-06-13 17:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.02%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int sum=0;
+        for(int i=0;i<columnTitle.size();i++){
+            sum=sum*26+(columnTitle[i]-'A'+1);
+        }
+
+        return sum;
+    }
+};
