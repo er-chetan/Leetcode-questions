17869332a890ad12b4a1adec85c0421b1a54a44b
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-07-11 00:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (11.59%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+       
+        if(root1 == NULL || root2 == NULL){
+            return root1 == root2;
+        }
+        
+        //check condition fourth
+        if(root1->val != root2->val){
+            return 0;
+        }
+    
+        return ( flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left) )|| (flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right));
+    }
+};
