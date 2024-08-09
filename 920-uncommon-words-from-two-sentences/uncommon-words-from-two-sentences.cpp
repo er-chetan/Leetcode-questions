fc03df9c146f76bb17b2ac712a06e5105e1f70f3
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-08-09 23:55:44 +0530

    Time: 3 ms (43.92%) | Memory: 8.9 MB (52.54%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string,int> m1,m2;
+        string str="";
+        int n=s1.size();
+        int m=s2.size();
+        for(int i=0;i<n;i++){
+            if(isalpha(s1[i])){
+                str+=s1[i];
+            }
+            if(!isalpha(s1[i]) || i==n-1){
+                m1[str]++;
+                str="";
+            }   
+        }
+
+        for(int i=0;i<m;i++){
+            if(isalpha(s2[i])){
+                str+=s2[i];
+            }
+            if(!isalpha(s2[i]) || i==m-1){
+                m2[str]++;
+                str="";
+            }   
+        }
+
+        // for(auto ele : m1){
+        //     cout<<ele.first<<" "<<ele.second<<endl;
+        // }
+
+        // for(auto ele : m1){
+        //     cout<<ele.first<<" "<<ele.second<<endl;
+        // }
+
+        vector<string> v;
+        for(auto ele : m1){
+            if(m2.find(ele.first)==m2.end()){
+                if(ele.second==1){
+                    cout<<ele.first<<" "<<ele.second<<endl;
+                    v.push_back(ele.first);
+                }
+            }
+        }
+
+        cout<<endl;
+
+        for(auto ele : m2){
+            if(m1.find(ele.first)==m1.end()){
+                if(ele.second==1){
+                    cout<<ele.first<<" "<<ele.second<<endl;
+                    v.push_back(ele.first);
+                }
+            }
+        }
+
+        return v;
+    }
+};
