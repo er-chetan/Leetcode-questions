04472c6924b7a965dac328f541188c21c39ecde3
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-08-11 00:02:27 +0530

    Time: 8 ms (10.29%) | Memory: 12.1 MB (16.70%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.cpp
+++ 822-unique-morse-code-words/unique-morse-code-words.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        unordered_set<string> s;
-        vector<string> morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        string temp = "";
-        for(auto word : words){
-            temp = "";
-            for(auto c : word){
-                temp += morse[c - 'a'];
-            }
-            s.insert(temp);
+        unordered_map<char,string> m;
+        vector<string> v={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        for(int i=0;i<26;i++){
+            m[char(97+i)]=v[i];
         }
-        return s.size();
+        cout<<endl;
+        unordered_set<string> st;
+        for(int i=0;i<words.size();i++){
+            string str=words[i];
+            string s="";
+            for(int j=0;j<str.size();j++){
+                s+=m[str[j]];
+            }
+            st.insert(s);
+        }
+
+        for(auto ele : st){
+            cout<<ele<<" ";
+        }
+
+
+        return st.size();
     }
 };
