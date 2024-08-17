e12c39f0386fb0e3858e5946db0c2a0fe1256773
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-08-18 00:05:37 +0530

    Time: 31 ms (6.75%) | Memory: 21.1 MB (64.82%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -10,20 +10,38 @@
  * };
  */
 class Solution {
-    TreeNode* ans;
-    int leafDepth(TreeNode* root, int d, int &maxD) {
-        if(!root) return d - 1;
-        int l = leafDepth(root -> left, d + 1, maxD);
-        int r = leafDepth(root -> right, d + 1, maxD);
-        int leafD = max(l, r);
-        maxD = max(maxD, leafD);
-        if(maxD == l && l == r) ans = root;
-        return leafD;
-    }
 public:
+    TreeNode* temp;
+    int maxdep=INT_MIN;
+    int helper(TreeNode* root,int depth){
+        if(root==NULL) return depth;
+        depth++;
+        int left = helper(root->left,depth);
+        int right=helper(root->right,depth);
+        cout<<root->val<<"="<<left<<" "<<right<<endl;
+        if(left==right && depth<left){
+            if(maxdep<=left){
+                temp=root;
+                maxdep=left;
+                // cout<<temp->val<<"="<<left<<"+"<<right<<" ";
+            } 
+        }else if(left>right  && maxdep<left){
+            maxdep=left;
+            temp=root->left;
+            // cout<<temp->val<<"="<<left<<"+"<<right<<" ";
+        }else if(left<right && maxdep<right){
+            maxdep=right;
+            temp=root->right;
+            // cout<<temp->val<<"="<<left<<"+"<<right<<" ";
+        } 
+        return max(left,right);
+    }
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        int maxD = 0;
-        leafDepth(root, 0, maxD);
-        return ans;
+       int res= helper(root,0);
+       if(res==1){
+        return root;
+       }
+    //    cout<<"last->"<<temp->val;
+       return temp;
     }
 };
