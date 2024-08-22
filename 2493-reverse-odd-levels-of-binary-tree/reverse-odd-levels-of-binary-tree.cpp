44af004c2bb105d4136940213769c2c761fa8746
Chetan <99252948+er-chetan@users.noreply.github.com>
2024-08-22 23:37:58 +0530

    Time: 219 ms (5.40%) | Memory: 78.4 MB (23.50%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -18,24 +18,24 @@ public:
         while(q.size()>0){
             int n=q.size();
             if(level%2!=0){
-                    vector<int> r;
-                    for(int i=0;i<n;i++){
-                        TreeNode* t=q.front();
-                        cout<<t->val<<" "; 
-                        q.pop();
-                        r.push_back(t->val);
-                        q.push(t);
-                    }
-                    cout<<endl;
-                    for(int i=n-1;i>=0;i--){
-                        TreeNode* t=q.front();
-                        cout<<t->val<<" "; 
-                        q.pop();
-                        t->val=r[i];
-                        q.push(t);
-                    }
-
+                vector<int> r;
+                for(int i=0;i<n;i++){
+                    TreeNode* t=q.front();
+                    cout<<t->val<<" "; 
+                    q.pop();
+                    r.push_back(t->val);
+                    q.push(t);
+                }     
+                cout<<endl;
+                for(int i=n-1;i>=0;i--){
+                    TreeNode* t=q.front();
+                    cout<<t->val<<" "; 
+                    q.pop();
+                    t->val=r[i];
+                    q.push(t);
                 }
+
+            }                   
             for(int i=0;i<n;i++){
                 TreeNode* temp=q.front();
                 q.pop();
