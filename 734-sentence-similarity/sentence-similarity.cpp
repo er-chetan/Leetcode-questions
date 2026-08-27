d7eb77c2ba5cb0d01e6bcfc90a458f01378b8951
Chetan <99252948+er-chetan@users.noreply.github.com>
2026-08-27 22:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (74.74%) - LeetSync

---
+++ 734-sentence-similarity/sentence-similarity.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool areSentencesSimilar(vector<string>& sentence1, vector<string>& sentence2, vector<vector<string>>& similarPairs) {
+       set<pair<string,string>> s;
+       if(sentence1.size()!=sentence2.size()){
+        return false;
+       }
+
+       for(int i=0;i<similarPairs.size();i++){
+        s.insert({similarPairs[i][0],similarPairs[i][1]});
+       }
+       for(int i=0;i<similarPairs.size();i++){
+        s.insert({similarPairs[i][1],similarPairs[i][0]});
+       }
+    
+        int count=0;
+       for(int i=0;i<sentence1.size();i++){
+            if(sentence1[i]==sentence2[i]){
+                count++;
+                continue;
+            }else if(s.find({sentence1[i],sentence2[i]})!=s.end()){
+                count++;
+                continue;
+            }
+            else if(s.find({sentence2[i],sentence1[i]})!=s.end()){
+                count++;
+                continue;
+            }
+       }
+        cout<<count<<" "<<sentence1.size();
+        if(count==sentence1.size()) return true;
+       
+
+        return false;
+    }
+};
