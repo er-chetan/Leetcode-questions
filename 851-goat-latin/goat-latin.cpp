3473d9d426b40caff4c609df368d847dae2c4087
Chetan <99252948+er-chetan@users.noreply.github.com>
2026-08-31 09:04:09 +0530

    Time: 1 ms (32.87%) | Memory: 10.3 MB (5.11%) - LeetSync

---
+++ 851-goat-latin/goat-latin.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string toGoatLatin(string S) {
+        unordered_set<char> vowel({'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'});
+        int n =S.size(),i=0;
+        stringstream s(S);
+        string temp,res;
+        int count=0;
+        while(s>> temp){
+            count++;
+
+            if(vowel.find(temp[0])!=vowel.end()){
+                temp=temp+"ma";
+            }else{
+                char c=temp[0];
+                temp=temp.substr(1);
+                temp =temp+c+"ma";
+            }
+
+            for(int i=0;i<count;i++){
+                temp.push_back('a');
+            }
+            res=res+temp;
+            res=res+' ';
+
+        }
+
+        res.pop_back();
+       
+       cout<<res;
+
+       
+
+        return res;
+    }
+};
