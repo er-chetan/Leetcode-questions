f813f96b30cb4ad103a47c92536f912ebaf008e1
Chetan <99252948+er-chetan@users.noreply.github.com>
2026-09-06 23:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (55.00%) - LeetSync

---
+++ 3577-convert-doubly-linked-list-to-array-i/convert-doubly-linked-list-to-array-i.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for doubly-linked list.
+ * class Node {
+ *     int val;
+ *     Node* prev;
+ *     Node* next;
+ *     Node() : val(0), next(nullptr), prev(nullptr) {}
+ *     Node(int x) : val(x), next(nullptr), prev(nullptr) {}
+ *     Node(int x, Node *prev, Node *next) : val(x), next(next), prev(prev) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> toArray(Node *head){
+        vector<int> v;
+        Node* temp=head;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            cout<<temp->val<<" ";
+            temp=temp->next;
+        }
+
+
+
+        return v;
+    }
+};
