7fe1879891d86fa8bd7a5af07a60a6a19d8c38e1
Chetan <99252948+er-chetan@users.noreply.github.com>
2026-09-20 17:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (79.60%) - LeetSync

--- 1074-high-five/high-five.cpp
+++ 1074-high-five/high-five.cpp
@@ -5,15 +5,9 @@ public:
         vector<vector<int>> ans;
         sort(items.begin(),items.end());
         reverse(items.begin(),items.end());
-        // int num=-1;
-        // int sum=0;
         int count=0;
-        // int num_prev=-1
         for(int i=0;i<items.size();i++){
-                // cout<<count<<" ";
                 if(count==5 && m.find(items[i][0])!=m.end()){
-                    // cout<<"y"<<" ";
-                    // count=0;
                     continue;
                 }
 
@@ -23,18 +17,15 @@ public:
                 
                 if(m.find(items[i][0])!=m.end()){
                     m[items[i][0]]+=items[i][1];
-                    // cout<<m[items[i][0]]<<" "<<items[i][0]<<endl;
                     count++;
                 }else if(count==0 && m.find(items[i][0])==m.end()) {
                     m[items[i][0]]=items[i][1];
-                    // cout<<m[items[i][0]]<<" "<<items[i][0]<<endl;
                     count++;    
                 }
 
         }
 
         for(auto ele : m){
-            // cout<<ele.first<<" "<<ele.second<<endl;
             ans.push_back({ele.first,ele.second/5});
         }
 
