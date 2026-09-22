fe20fb95b8e401503e7a58ec0cc0e9f7af75833c
Chetan <99252948+er-chetan@users.noreply.github.com>
2026-09-23 00:10:54 +0530

    Time: 444 ms (5.41%) | Memory: 9.6 MB (25.68%) - LeetSync

---
+++ 1123-single-row-keyboard/single-row-keyboard.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calculateTime(string keyboard, string word) {
+        unordered_map<char,int> m;
+
+        for(int i=0;i<keyboard.size();i++){
+            m[keyboard[i]]=i;
+        }
+
+        int prev=-1;
+        int sum=0;
+        for(int i=0;i<word.size();i++){
+
+            if(m.find(word[i])!=m.end()){
+                
+                if(prev==-1){
+                    
+                    prev=m[word[i]];
+                    sum=prev;
+                    cout<<sum<<" "<<prev<<endl;
+                }else{
+                    sum=sum+abs(prev-m[word[i]]);
+                    prev=m[word[i]];
+                    cout<<sum<<" "<<m[word[i]]<<endl;
+                }
+            }
+
+            
+        }
+        cout<<"sum"<<sum;
+        return sum;
+    }
+};
