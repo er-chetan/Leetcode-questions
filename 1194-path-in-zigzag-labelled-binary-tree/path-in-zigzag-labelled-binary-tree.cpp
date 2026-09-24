b42a632e7c9dd15d02bc310b2ac7d2248a3e2d6b
Chetan <99252948+er-chetan@users.noreply.github.com>
2026-09-24 19:33:52 +0530

    Time: 31 ms (5.09%) | Memory: 8.6 MB (14.91%) - LeetSync

---
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        int height=0;
+        vector<int> ans;
+        for(int i=1;i<label;i++){
+            if(label>=pow(2,i)){
+                height++;
+            }
+        }
+        cout<<height<<" h";
+        // h--
+        ans.push_back(label);
+        while(height){
+            int mini_value=pow(2,height-1);// previous level
+            int maxi_value=pow(2,height)-1;// previous level
+            label=mini_value+maxi_value-(label/2);
+            cout<<label<<" ";
+            ans.push_back(label);
+            height--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
